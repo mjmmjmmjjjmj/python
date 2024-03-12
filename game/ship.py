69891efb7af83bbc3d914cf69aefc3425f027d6c
mjmmjmmjjjmj <160682854+mjmmjmmjjjmj@users.noreply.github.com>
2024-03-12 17:59:35 +0900

    Create ship.py

---
+++ game/ship.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280,720))
+
+clock = pygame.time.Clock()
+image = pygame.image.load('./images/ship.bmp')
+rect = image.get_rect()
+
+while True:
+    # Process player inputs.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Do logical updates here.
+    # ...
+
+    screen.fill("purple")  # Fill the display with a solid color
+
+    # Render the graphics here.
+    # ...
+    screen.blit(image, rect)
+
+    pygame.display.flip()  # Refresh on-screen display
+    clock.tick(60)         # wait until next frame (at 60 FPS)
